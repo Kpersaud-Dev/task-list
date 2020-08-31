c4368cca83c418ecfb5db50ada03370ff4076368
kevin_p <kprsd14@gmail.com>
2020-08-31 16:46:51 -0400

    Added filter to task list

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ loadEventListeners();
 function loadEventListeners() {
   //Add Task Event
   form.addEventListener('submit', addTask);
+  //Remove task event
+  taskList.addEventListener('click', removeTask);
+  //Clear task event
+  clearBtn.addEventListener('click', clearTasks);
+  //Filter Tasks event
+  filter.addEventListener('keyup', filterTasks);
 }
 
 //Add Task
@@ -45,4 +51,38 @@ function addTask(e) {
 
 
   e.preventDefault();
+}
+
+//Remove Task
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains('delete-item')) {
+    if (confirm('Are You Sure?')) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+//Clear Tasks
+function clearTasks() {
+  // taskList.innerHTML = '';
+
+  //Faster
+
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+//Filter Tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(function (task) {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
 }
